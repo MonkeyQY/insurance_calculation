9f75645748bf1b959bee990212f80d9cde0be879
monkey <vasilevskiy.job@gmail.com>
2023-07-15 16:31:07 +0300

    add raises to tests

--- tests/test_insurance/test_services.py
+++ tests/test_insurance/test_services.py
@@ -1,8 +1,10 @@
 from _decimal import Decimal
 from datetime import datetime
 from unittest.mock import AsyncMock
+from contextlib import nullcontext as does_not_raise
 
 import pytest
+from fastapi import HTTPException
 
 from app.insurance.schemas import CalculateRequest
 from app.insurance.services import calculate_insurance, get_current_rate
@@ -16,22 +18,60 @@ from app.insurance.services import calculate_insurance, get_current_rate
         (Decimal("1000"), Decimal("0.02"), Decimal("20")),
         (Decimal("1000"), Decimal("0.03"), Decimal("30")),
         (Decimal("1000"), Decimal("0.04"), Decimal("40")),
-    ]
+    ],
 )
-async def test_calculate_insurance(declared_value: Decimal, rate: Decimal, expected: Decimal):
+async def test_calculate_insurance(
+    declared_value: Decimal, rate: Decimal, expected: Decimal
+):
     assert await calculate_insurance(declared_value, rate) == expected
 
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
-    "data, insurance",
+    "data, insurance, expectation",
     [
-        (CalculateRequest(date=datetime.today(), cargo_type="Other", declared_value=Decimal("1000")), Decimal("0.01")),
-        (CalculateRequest(date=datetime.today(), cargo_type="Other", declared_value=Decimal("1000")), Decimal("0.01")),
-        (CalculateRequest(date=datetime.today(), cargo_type="Other", declared_value=Decimal("1000")), Decimal("0.01")),
-    ]
+        (
+            CalculateRequest(
+                date=datetime.today(),
+                cargo_type="Other",
+                declared_value=Decimal("1000"),
+            ),
+            Decimal("0.01"),
+            does_not_raise(),
+        ),
+        (
+            CalculateRequest(
+                date=datetime.today(),
+                cargo_type="Other",
+                declared_value=Decimal("1000"),
+            ),
+            Decimal("0.01"),
+            does_not_raise(),
+        ),
+        (
+            CalculateRequest(
+                date=datetime.today(),
+                cargo_type="Other",
+                declared_value=Decimal("1000"),
+            ),
+            Decimal("0.01"),
+            does_not_raise(),
+        ),
+        (
+            CalculateRequest(
+                date=datetime.today(),
+                cargo_type="Other",
+                declared_value=Decimal("1000"),
+            ),
+            None,
+            pytest.raises(HTTPException),
+        ),
+    ],
 )
-async def test_get_current_rate(data: CalculateRequest, insurance: Decimal):
+async def test_get_current_rate(
+    data: CalculateRequest, insurance: Decimal, expectation
+):
     mock_manager = AsyncMock()
     mock_manager.get_rate_for_date_and_type.return_value = insurance
-    assert await get_current_rate(data, mock_manager) == insurance
+    with expectation:
+        assert await get_current_rate(data, mock_manager) == insurance
